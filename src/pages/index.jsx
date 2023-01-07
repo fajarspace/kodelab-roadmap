2e1684cb560f23ac48d062c4d03d6258fa1eb6e8
Fajar Agung <fajar.agngn@gmail.com>
2023-01-08 00:12:05 +0700

    fixing netlify build

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
+// import clsx from 'clsx';
+// import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomePage from "../components/HomePage";
